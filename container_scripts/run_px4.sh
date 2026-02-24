d0077e669c996db2ccc4e25a611439c578ad5028
RaphaelBonaccorsi <raphaelbonaccorsi@usp.br>
2026-02-24 11:05:16 -0300

    chore: update PX4 home coordinates

--- container_scripts/run_px4.sh
+++ container_scripts/run_px4.sh
@@ -2,8 +2,6 @@
 
 cd /home/PX4-Autopilot
 
-export PX4_HOME_LAT=-22.001333
-export PX4_HOME_LON=-47.934152
-export PX4_HOME_ALT=847.142652
-
+export PX4_HOME_LAT=-8.368936009367731;
+export PX4_HOME_LON=-35.014375441995696;
 make px4_sitl gazebo-classic HEADLESS=1
