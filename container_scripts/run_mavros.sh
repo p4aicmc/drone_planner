4f526f4f9cb131182eef34b2a2e73eab8568f13e
RaphaelBonaccorsi <raphaelbonaccorsi@usp.br>
2026-03-19 14:23:32 -0300

    Updates container build to Ubuntu 24.04, ROS Jazzy and Gazebo Harmonic

--- container_scripts/run_mavros.sh
+++ container_scripts/run_mavros.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /opt/ros/humble/setup.sh 
+source /opt/ros/jazzy/setup.sh 
 ros2 launch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14580"
--- container_scripts/run_px4.sh
+++ container_scripts/run_px4.sh
@@ -4,4 +4,5 @@ cd /home/PX4-Autopilot
 
 export PX4_HOME_LAT=-8.368936009367731;
 export PX4_HOME_LON=-35.014375441995696;
-make px4_sitl gazebo-classic HEADLESS=1
+export PX4_HOME_ALT=0.0;
+make px4_sitl gz_x500 HEADLESS=1
